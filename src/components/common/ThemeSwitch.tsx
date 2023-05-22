5d8aca07ddbd94372f57903ced3cadfbffaa0538
Sajad Shafi <maliksajad004@gmail.com>
2023-05-22 18:40:27 +0530

    AddedThemeContext

---
+++ src/components/common/ThemeSwitch.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ThemeSwitch = () => {
+  return (
+    <div className="checkbox-wrapper-5">
+      <div className="check">
+        <label htmlFor="check-5">
+          <input
+            type="checkbox"
+            id="check-5"
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default ThemeSwitch;
