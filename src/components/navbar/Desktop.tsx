bd760bdc382b99d5a6309d3222f6038d2447744f
Sajad Shafi <maliksajad004@gmail.com>
2023-07-18 19:04:23 +0530

    fixed navbar issues

---
+++ src/components/navbar/Desktop.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import React from 'react';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import routes from '@/utils/routes';
+import ThemeSwitch from '../ThemeSwitch';
+import useTheme from '@/store/ThemeContext';
+import useYScroll from '@/hooks/useYScroll';
+
+const DesktopNav = () => {
+  const currentRoute = usePathname();
+  const { scrollProgressY } = useYScroll();
+  const { theme } = useTheme();
+
+  return (
+    <nav
+      className={`lg:flex z-50 items-center hidden justify-between px-4 h-[122px]  ${
+        scrollProgressY > 130 &&
+        'h-[90px] animate-slideDown backdrop-blur-md dark:shadow-shadow1 bg-backgroundColor1 shadow-shadowNavLight dark:bg-bgGlassDark top-0 sticky'
+      }`}>
+      <Image
+        className="w-[140px] h-auto"
+        src={
+          theme === 'dark'
+            ? '/images/logo-light-200.png'
+            : '/images/logo-dark-200.png'
+        }
+        width={140}
+        height={70}
+        alt="Sajad Shafi - Professional Software Engineer"
+      />
+      <ul className="flex items-center">
+        {routes &&
+          routes.map(route => (
+            <li
+              key={route.to}
+              className="mx-[10px] my-[5px] ">
+              <Link
+                className={`uppercase transition-colors duration-400ms hover:text-colorPrimary dark:hover:text-colorPrimary font-semibold font-primary dark:font-medium dark:hover:font-semibold text-[13px] px-[10px] leading-lineHeightb1 py-[5px] block ${
+                  currentRoute === route.to
+                    ? 'text-colorPrimary'
+                    : 'dark:text-colorLightn dark:font-semibold text-colorBodyWhite '
+                }`}
+                href={route.to}>
+                {route.text}
+              </Link>
+            </li>
+          ))}
+        <div className="mx-4 flex justify-end">
+          <Link
+            className="hire links-desktop"
+            href="/">
+            <span>HIRE ME</span>
+          </Link>
+        </div>
+        <div className="flex items-center ml-5">
+          <ThemeSwitch />
+        </div>
+      </ul>
+    </nav>
+  );
+};
+
+export default DesktopNav;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import Mobile from './Mobile';
+import Desktop from './Desktop';
+
+const Navbar = () => {
+  return (
+    <>
+      <Desktop />
+      <Mobile />
+    </>
+  );
+};
+
+export default Navbar;
